e5c74695b3fbc50c312b6d10b0c44dea60f03777
Aziza <aziz2000_@hotmail.com>
2026-05-31 03:49:58 +0300

    Fix get_user_by_email function signature

--- app/crud/user.py
+++ app/crud/user.py
@@ -16,8 +16,6 @@ def create_user(db: Session, user):
 def get_user(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
-def get_user_by_email(db, email: str):
-    return db.query(User).filter(User.email == email).first(
-        
-    )
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
 
