8afd6a8dd422a7f048fd2e0eeea29c0a2d10ea9f
Aziza <aziz2000_@hotmail.com>
2026-05-31 03:52:27 +0300

    Update Message model for private chat support

--- app/models/message.py
+++ app/models/message.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, DATETIME
 from app.db.base_class import Base
 from datetime import datetime
 
 
 class Message(Base):
-    __tablename__= "messages"
-    
+    __tablename__ = "messages"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    content = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    from_user = Column(String, index=True, nullable=False)
+    to_user   = Column(String, index=True, nullable=False)
+    content   = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, index=True)
