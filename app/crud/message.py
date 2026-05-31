4f090772d6754127960da50da383f5e205c88049
Aziza <aziz2000_@hotmail.com>
2026-05-31 03:49:26 +0300

    Support sender and receiver in message creation

--- app/crud/message.py
+++ app/crud/message.py
@@ -1,7 +1,8 @@
 from app.models.message import Message
 
-def create_message(db, username: str, content: str):
-    msg = Message(username=username, content=content)
+
+def create_message(db, from_user: str, to_user: str, content: str) -> Message:
+    msg = Message(from_user=from_user, to_user=to_user, content=content)
     db.add(msg)
     db.commit()
     db.refresh(msg)
