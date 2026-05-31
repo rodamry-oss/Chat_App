51deb39a5ac1fbecf00c6fedcc759440c8e8b20a
Aziza <aziz2000_@hotmail.com>
2026-05-31 03:38:48 +0300

    Use settings for JWT token expiration

--- app/core/security.py
+++ app/core/security.py
@@ -19,7 +19,10 @@ def verify_password(password, hashed):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
 
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
